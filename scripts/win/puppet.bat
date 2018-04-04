76e37fc28af23bead43f7cc14f631538479e9bef
lmayorga <lmayorga@lm3corp.com>
2018-04-04 13:57:50 -0400

    build a windows ami from amazon ami

--- scripts/win/puppet.bat
+++ scripts/win/puppet.bat
@@ -1,21 +1,8 @@
 
-if "%PUPPET_RELEASE%" equ "open" goto :opensource
 if "%PUPPET_RELEASE%" equ "enterprise" goto :enterprise
 if "%PUPPET_RELEASE%" equ "none" goto :none
 
 goto :done
-
-:opensource
-
-if not exist "C:\Windows\Temp\puppet.msi" (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://nuget.nreca.org/puppet/puppet-agent-%PUPPET_VERSION%-x86.msi', 'C:\Windows\Temp\puppet.msi')" <NUL
-)
-
-msiexec /qn /i C:\Windows\Temp\puppet.msi PUPPET_AGENT_STARTUP_MODE=disabled PUPPET_MASTER_SERVER=puppetmaster.localdomain  /log C:\Windows\Temp\puppet.log
-
-<nul set /p ".=;C:\Program Files (x86)\Puppet Labs\Puppet\bin" >> C:\Windows\Temp\PATH
-set /p PATH=<C:\Windows\Temp\PATH
-setx PATH "%PATH%" /m
 
 :enterprise
 
